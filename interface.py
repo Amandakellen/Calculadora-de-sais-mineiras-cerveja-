bcdde5b4bf1d3ab3925a613c7b1eb7a2016725a8
Amanda Kellen <49374780+amandakellen@users.noreply.github.com>
2020-09-11 13:03:36 -0300

    adicionando botão

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import Calculadora_de_Sais
+from tkinter import messagebox
 
 class interface:
     def __init__(self):
@@ -18,21 +19,21 @@ class interface:
         self.frame_agua=Frame(self.janela_principal)
         self.frame_litros=Frame(self.janela_principal,bg='white')
         self.frame_espaco=Frame(self.janela_principal)
-        self.frame_lavagem=Frame(self.janela_principal)
+        self.frame_lavagem=Frame(self.janela_principal,bg='gray46')
         self.frame_mostura=Frame(self.janela_principal)
 
         #Adicionando Labels da janela principal
         self.label_nome=Label(self.frame_titulo,height=2,width=28,fg='white',bg='gray26',text="Zalino",font=('arial black',18),anchor=CENTER)
         self.label_litros=Label(self.frame_litros,height=2,width=18,fg='white',bg='gray38',text="Quantidade de Litros",anchor=W)
-        self.label_lavagem=Label(self.frame_lavagem,height=2,width=18,fg='white',bg='gray46',text="Água de Lavagem",anchor=CENTER)
-        self.label_espaco=Label(self.frame_espaco,height=2,width=18,bg='white')
+        self.label_lavagem=Label(self.frame_lavagem,height=3,width=18,fg='white',bg='gray46',relief=FLAT,text="Água de Lavagem",anchor=CENTER)
+        self.label_espaco=Label(self.frame_espaco,height=4,width=18,bg='white')
+
         #Adicionando Entry na janela principal
-        self.retorna = self.frame_litros.register(self.validar_dado)
         self.entry_litros=Entry(self.frame_litros,bd=0.5,width=28,font='Calibri',textvariable=self.litros)
-        self.entry_litros.bind(self.litros, (lambda _: self.validar_dado()))
+
 
         #Adicionando Botão
-
+        self.botao_lavagem=Button(self.frame_lavagem,text='Selecionar',command=self.validar_dado)
 
 
         #Empacotando dados da janela principal
@@ -42,9 +43,11 @@ class interface:
         self.label_espaco.pack(side=TOP)
         self.label_lavagem.pack(side=TOP)
 
-
         #Entry
         self.entry_litros.pack(side=RIGHT)
+
+        #Botão
+        self.botao_lavagem.pack()
         #Frame
         self.frame_titulo.pack(side=TOP)
         self.frame_litros.pack(side=TOP,expand=False,fill =X)
@@ -54,15 +57,30 @@ class interface:
         self.janela_principal.mainloop()
 
     def validar_dado(self):
-            try:
-                float(self.litros.get())
-                print('oi')
 
-            except ValueError:
-                print('5')
+        if self.litros.get() !="":
+           if self.litros.get().isalpha():
+               self.entry_litros.delete(0,len(self.litros.get()))
+               self.litros.set("")
+               messagebox.showerror("ERRO!","Valor digitado não é válido!")
+
+           elif self.litros.get().isspace():
+               self.entry_litros.delete(0, len(self.litros.get()))
+               self.litros.set("")
+               messagebox.showerror("ERRO!", "Valor digitado não é válido!")
+           else:
+               try:
+                   float(self.litros.get())
+               except:
+                   self.entry_litros.delete(0, len(self.litros.get()))
+                   self.litros.set("")
+                   messagebox.showerror("ERRO!", "Valor digitado não é válido!")
+    #def janela_lavagem(self):
+
+
+
 if __name__ == '__main__':
     obj=interface()
-    obj.validar_dado()
     print('fim')
 
 
