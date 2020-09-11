32731deea20fe1f12034b61da55b9c2d2cfe70b5
Amanda Kellen <49374780+amandakellen@users.noreply.github.com>
2020-09-11 11:58:03 -0300

    adicionando lógica de validação

--- interface.py
+++ interface.py
@@ -10,33 +10,59 @@ class interface:
         self.janela_principal.resizable(False, False)
         self.janela_principal.config(bg='white')
 
+        #Variáveis do Entry
+        self.litros=StringVar()
+
         #Criando frames da janela principal
         self.frame_titulo=Frame(self.janela_principal)
         self.frame_agua=Frame(self.janela_principal)
-        self.frame_litros=Frame(self.janela_principal)
+        self.frame_litros=Frame(self.janela_principal,bg='white')
+        self.frame_espaco=Frame(self.janela_principal)
         self.frame_lavagem=Frame(self.janela_principal)
         self.frame_mostura=Frame(self.janela_principal)
 
         #Adicionando Labels da janela principal
         self.label_nome=Label(self.frame_titulo,height=2,width=28,fg='white',bg='gray26',text="Zalino",font=('arial black',18),anchor=CENTER)
-        self.label_litros=Label(self.frame_litros,height=2,width=28,fg='white',bg='gray38',text="Quantidade de Litros",anchor=E)
-        #Empacotando dados da janela principal
-        self.label_nome.pack(side=TOP)
+        self.label_litros=Label(self.frame_litros,height=2,width=18,fg='white',bg='gray38',text="Quantidade de Litros",anchor=W)
+        self.label_lavagem=Label(self.frame_lavagem,height=2,width=18,fg='white',bg='gray46',text="Água de Lavagem",anchor=CENTER)
+        self.label_espaco=Label(self.frame_espaco,height=2,width=18,bg='white')
+        #Adicionando Entry na janela principal
+        self.retorna = self.frame_litros.register(self.validar_dado)
+        self.entry_litros=Entry(self.frame_litros,bd=0.5,width=28,font='Calibri',textvariable=self.litros)
+        self.entry_litros.bind(self.litros, (lambda _: self.validar_dado()))
 
-        self.frame_titulo.pack()
-        self.frame_litros.pack()
+        #Adicionando Botão
+
+
+
+        #Empacotando dados da janela principal
+        #label
+        self.label_nome.pack(side=TOP)
+        self.label_litros.pack(side=LEFT)
+        self.label_espaco.pack(side=TOP)
+        self.label_lavagem.pack(side=TOP)
+
+
+        #Entry
+        self.entry_litros.pack(side=RIGHT)
+        #Frame
+        self.frame_titulo.pack(side=TOP)
+        self.frame_litros.pack(side=TOP,expand=False,fill =X)
+        self.frame_espaco.pack()
         self.frame_lavagem.pack()
         self.frame_mostura.pack()
         self.janela_principal.mainloop()
 
-    def janela_calculo(self):
-        self.janela_calculo=Toplevel()
-        self.janela_calculo.title('Zalino')
-        self.janela_calculo.geometry('350x500')
-        self.janela_calculo.resizable(False, False)
-        self.janela_calculo.config(bg='white')
+    def validar_dado(self):
+            try:
+                float(self.litros.get())
+                print('oi')
 
-
+            except ValueError:
+                print('5')
 if __name__ == '__main__':
     obj=interface()
+    obj.validar_dado()
+    print('fim')
+
 
